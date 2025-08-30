3a1204f1281cfa1c857c4eab6078c219d12652d3
AyrtonSur <ayrtonsurica@gmail.com>
2025-08-30 16:32:09 -0300

    build: initial commit

---
+++ src/test/java/com/war/game/war_backend/WarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.war.game.war_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
