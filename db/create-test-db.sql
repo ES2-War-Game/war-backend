628e972dd700557d03ee15e58ffa6f452f202d48
AyrtonSur <ayrtonsurica@gmail.com>
2025-09-11 09:04:58 -0300

    fix: retirando arquivos não utilizados e voltando o docker ao original

--- db/create-test-db.sql
+++
@@ -1,5 +0,0 @@
--- db/create-test-db.sql
-CREATE DATABASE wargame_test;
-\connect wargame_test;
-CREATE USER wargame WITH PASSWORD 'wargame';
-GRANT ALL PRIVILEGES ON DATABASE wargame_test TO wargame;
