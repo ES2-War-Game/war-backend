c44c3aa5a23ef4700e975c2bc98608d119878232
AyrtonSur <ayrtonsurica@gmail.com>
2025-09-22 12:41:16 -0300

    fix: atualizando configuração do cors para aceitar somente urls definidas

--- src/main/java/com/war/game/war_backend/config/SecurityConfig.java
+++ src/main/java/com/war/game/war_backend/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // Mais permissivo para desenvolvimento
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000")); // Mais permissivo para desenvolvimento
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
